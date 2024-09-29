8d0755a944abb1b1d5cbb144fac1843b9938ab90
Pedro Marçal Ballesteros <54600506+Netroxx@users.noreply.github.com>
2024-09-29 13:16:30 -0700

    Update CarSpace.java

--- codigo/src/entities/CarSpace.java
+++ codigo/src/entities/CarSpace.java
@@ -3,5 +3,10 @@ package entities;
 public class CarSpace {
      private String idVaga;
     private boolean ehUtilizada;
+
+     public Vaga(String idVaga) {
+        this.idVaga = idVaga;
+        this.ehUtilizada = false; 
+    }
     
 }
