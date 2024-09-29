03597f2470f2c82ceb268d895a1e9423ad7254b1
Pedro Marçal Ballesteros <54600506+Netroxx@users.noreply.github.com>
2024-09-29 17:36:26 -0300

    CarSpace commit

--- codigo/src/entities/CarSpace.java
+++ codigo/src/entities/CarSpace.java
@@ -1,12 +1,51 @@
 package entities;
 
-public class CarSpace {
-     private String idVaga;
-    private boolean ehUtilizada;
+public class ParkingSpot {
 
-     public Vaga(String idVaga) {
-        this.idVaga = idVaga;
-        this.ehUtilizada = false; 
-    }
+    private String spotId;
+    private boolean isOccupied;
+
     
+    public ParkingSpot(String spotId) {
+        this.spotId = spotId;
+        this.isOccupied = false; 
+    }
+
+ 
+    public String getSpotId() {
+        return spotId;
+    }
+
+    public void setSpotId(String spotId) {
+        this.spotId = spotId;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean isOccupied) {
+        this.isOccupied = isOccupied;
+    }
+
+    
+    public void allocateSpot(Rental rental) {
+        if (!isOccupied) {
+            this.isOccupied = true;
+            System.out.println("Spot allocated: " + this.spotId);
+            
+        } else {
+            System.out.println("Spot is already occupied.");
+        }
+    }
+
+    public void freeSpot() {
+        if (isOccupied) {
+            this.isOccupied = false;
+            System.out.println("Spot freed: " + this.spotId);
+        } else {
+            System.out.println("Spot is already available.");
+        }
+    }
 }
+
