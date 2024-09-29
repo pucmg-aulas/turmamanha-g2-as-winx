40fa3794976972e26a2a231c619786cbde279b0a
Maria Clara <mariaclariagomes@gmail.com>
2024-09-29 16:24:19 -0300

    feat: Adicionar classe Park e funcionalidades relacionadas
    Adiciona a classe Park com os atributos numberParkingSpaces, carSpaces, pricePerHour, pricePerMinute e idPark. Também inclui os métodos construtor, getNumberParkingSpaces, setNumberParkingSpaces e addCarSpace para manipulação dos espaços de estacionamento do parque.

--- codigo/src/entities/Park.java
+++ codigo/src/entities/Park.java
@@ -2,5 +2,29 @@ package codigo.src.entities;
 
 public class Park {
        private int numberParkingSpaces;
-       private space
+       private CarSpace[] carSpaces;
+       private double pricePerHour;
+       private double pricePerMinute;
+       private int idPark;
+}
+public Park(int numberParkingSpaces, double pricePerHour, double pricePerMinute, int idPark) {
+       this.numberParkingSpaces = numberParkingSpaces;
+       this.pricePerHour = pricePerHour;
+       this.pricePerMinute = pricePerMinute;
+       this.idPark = idPark;
+       this.carSpaces = new CarSpace[numberParkingSpaces];
+}
+public int getNumberParkingSpaces() {
+       return numberParkingSpaces;
+}
+public void setNumberParkingSpaces(int numberParkingSpaces) {
+       this.numberParkingSpaces = numberParkingSpaces;
+}
+public void addCarSpace(CarSpace carSpace) {
+       for (int i = 0; i < carSpaces.length; i++) {
+              if (carSpaces[i] == null) {
+                     carSpaces[i] = carSpace;
+                     break;
+              }
+       }
 }
